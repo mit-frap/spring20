fc4ac7eb2b934f67543cd87ce5754e89c63b0425
Andres Erbsen <andreser@mit.edu>
2020-04-08 15:33:35 -0400

    ps8

---
+++ pset08_HoareLogic/Pset8Sig.v
@@ -0,0 +1,16 @@
+(* As you are defining the semantics, there is not much that can be checked
+* mechanically this time. However, we still list point values of the different
+* tasks here so that those who do not wish to look at them don't need to:
+
+- exec: 10%
+- hoare_triple: 10%
+- hoare_triple_big_step: 10%
+- add_two_inputs_ok: 10%
+- max3_ok: 10%
+- fibonacci_ok:
+  - 10% for inductive invariant
+  - 10% for proof
+- your program:
+  - 10% for spec
+  - 20% for proof
+ *)
